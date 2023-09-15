7d95a7684e9958d085e4b4f8632d27855c45f974
Chandra Shekhar <cshekhar@nvidia.com>
2023-09-15 17:48:51 +0530

    Adding enum Category as one of the filed in Expense class.

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -1,18 +1,26 @@
 
-import 'package:flutter/material.dart';
 import 'package:uuid/uuid.dart';
 
+// it will provide new uuid for each new expense we need to vall uuid.v4()
 const  uuid = Uuid();
+
+enum Category {
+  food, 
+  travel,
+  leisure,
+  work,
+}
 
 class Expense {
 
   // id is not required field as it will be generated at runtime.
 
-  Expense({required this.title, required this.amount, required this.date}) : id = uuid.v4();
+  Expense({required this.title, required this.amount, required this.date, required this.category}) : id = uuid.v4();
 
   final String id;
   final String title;
   final double amount;
   final DateTime date;
+  final Category category;
 
 }

7c5700fe617e642c8717ebffc4ef6c71e2bc8259
Chandra Shekhar <cshekhar@nvidia.com>
2023-09-15 23:09:18 +0530

    Adding TextField to allow user to enter the new Task

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -1,5 +1,6 @@
 import 'package:expense_tracker/widgets/expenses_list/expenses_list.dart';
 import 'package:expense_tracker/models/expense.dart';
+import 'package:expense_tracker/widgets/new_expense.dart';
 import 'package:flutter/material.dart';
 
 class Expenses extends StatefulWidget {
@@ -25,7 +26,7 @@ class _ExpensesState extends State<Expenses> {
 
   // function which will be triggerd when the + icon will be pressed
   void _openAddExpenseOverlay() {
-    showModalBottomSheet(context: context, builder: (ctx) => const Text('Modal bottom sheet'));
+    showModalBottomSheet(context: context, builder: (ctx) => const NewExpense());
   }
 
   @override

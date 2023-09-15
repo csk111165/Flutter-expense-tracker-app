0af2af31a86d9b0af4f32d4b4c3b5560a21c20a8
Chandra Shekhar <cshekhar@nvidia.com>
2023-09-15 17:59:47 +0530

    Creating dummy Expense data for testing

--- lib/expenses.dart
+++ lib/expenses.dart
@@ -1,3 +1,4 @@
+import 'package:expense_tracker/models/expense.dart';
 import 'package:flutter/material.dart';
 
 
@@ -9,6 +10,13 @@ class Expenses extends StatefulWidget {
 }
 
 class _ExpensesState extends State<Expenses> {
+
+  final List<Expense> _registeredExpenses = [
+
+    Expense(title: 'Flutter-Course', amount: 19, date: DateTime.now(), category: Category.work),
+    Expense(title: 'Cinema', amount: 19, date: DateTime.now(), category: Category.leisure),
+
+  ];
   @override
   Widget build(BuildContext context) {
     return const Scaffold(

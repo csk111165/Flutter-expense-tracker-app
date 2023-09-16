74328224b08b44058532f3ab7f3676d5902e828c
Chandra Shekhar <cshekhar@nvidia.com>
2023-09-16 21:54:08 +0530

    Adding dropdown and saving its value

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'package:expense_tracker/models/expense.dart';
 
-final formatter =  DateFormat.yMd(); 
+final formatter = DateFormat.yMd();
 
 class NewExpense extends StatefulWidget {
   const NewExpense({super.key});
@@ -15,6 +16,7 @@ class _NewExpenseState extends State<NewExpense> {
   final _titleController = TextEditingController();
   final _amountController = TextEditingController();
   DateTime? _selectedDate;
+  Category _selectedCategory = Category.leisure;
 
   void _presentDatePicker() async {
     // predefined screen/widget provided by flutterS
@@ -22,10 +24,10 @@ class _NewExpenseState extends State<NewExpense> {
     final firstDate = DateTime(now.year - 1, now.month, now.day);
 
     final pickedDate = await showDatePicker(
-      context: context, 
-      initialDate: now, 
-      firstDate: firstDate, 
-      lastDate: now);
+        context: context,
+        initialDate: now,
+        firstDate: firstDate,
+        lastDate: now);
 
     setState(() {
       _selectedDate = pickedDate;
@@ -67,26 +69,52 @@ class _NewExpenseState extends State<NewExpense> {
                   ),
                 ),
               ),
-              const SizedBox(width: 16,),
-               Expanded(
+              const SizedBox(
+                width: 16,
+              ),
+              Expanded(
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.end,
                   crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
                     // ! at the end of selecteDate suggests the flutter that the value won't be nulll
-                    Text(_selectedDate == null ? 'No date selected!': formatter.format(_selectedDate!)),
-                     IconButton(
-                      onPressed: _presentDatePicker, 
+                    Text(_selectedDate == null
+                        ? 'No date selected!'
+                        : formatter.format(_selectedDate!)),
+                    IconButton(
+                      onPressed: _presentDatePicker,
                       icon: const Icon(Icons.calendar_month),
-                      ),
-
+                    ),
                   ],
                 ),
               ),
             ],
           ),
+          SizedBox(height: 16,),
           Row(
             children: [
+              DropdownButton(
+                value: _selectedCategory,
+                items: Category.values
+                    .map(
+                      (category) => DropdownMenuItem(
+                        value: category,
+                        child: Text(
+                          category.name.toUpperCase(),
+                        ),
+                      ),
+                    ).toList(), //toList to convert iterable to list
+                onChanged: (value) {
+                  if(value == null)
+                  {
+                    return;
+                  }
+                  setState(() {
+                    _selectedCategory = value;
+                  });
+                },
+              ),
+              Spacer(),
               TextButton(
                 onPressed: () {
                   Navigator.pop(context);

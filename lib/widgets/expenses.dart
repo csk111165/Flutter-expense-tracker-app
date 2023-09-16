dc0efd7da00d9e22620095c8b00d51a7700f7fee
Chandra Shekhar <cshekhar@nvidia.com>
2023-09-17 02:31:46 +0530

    Introuding chart and finalizing the app

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -1,3 +1,4 @@
+import 'package:expense_tracker/widgets/chart/chart.dart';
 import 'package:expense_tracker/widgets/expenses_list/expenses_list.dart';
 import 'package:expense_tracker/models/expense.dart';
 import 'package:expense_tracker/widgets/new_expense.dart';
@@ -91,7 +92,9 @@ class _ExpensesState extends State<Expenses> {
       ),
       body: Column(
         children: [
-          const Text('The chart'),
+           Chart(
+            expenses: _registeredExpenses,
+            ),
           // here we need to wrap the ExpenseList widget with Expanded, as it is a list and Column widget complains that in that case
           Expanded(
               child: mainContent,

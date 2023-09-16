c74e98649ef6c32dad1dfbd89cdd51155ac2a663
Chandra Shekhar <cshekhar@nvidia.com>
2023-09-17 00:22:10 +0530

    Adding dark mode swithching theme based on the system

--- lib/main.dart
+++ lib/main.dart
@@ -5,11 +5,31 @@ var kColorScheme = ColorScheme.fromSeed(
   seedColor: const Color.fromARGB(255, 96, 59, 181),
   );
 
+var kDarkColorScheme = ColorScheme.fromSeed(
+  brightness: Brightness.dark,
+  seedColor: const Color.fromARGB(255, 5, 99, 125),
+  );
+
 void main() {
 
   runApp(
      MaterialApp(
       debugShowCheckedModeBanner: false, // this will hide the debug banner watermark
+      // darktheme
+      darkTheme: ThemeData.dark().copyWith(
+        useMaterial3: true,
+        colorScheme: kDarkColorScheme,
+        cardTheme: const CardTheme().copyWith(
+          color: kDarkColorScheme.secondaryContainer,
+          margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kDarkColorScheme.primaryContainer,
+            foregroundColor: kDarkColorScheme.onPrimaryContainer,
+          ),
+        ),
+      ),
       // using Material 3 , it allows good look and feel
       theme: ThemeData().copyWith(
         useMaterial3: true,
@@ -37,6 +57,7 @@ void main() {
         ),
 
         ),
+      // controls the mode of the systemd dark or normal
       home: const Expenses(),
       ),
   );
